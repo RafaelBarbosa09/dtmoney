57aa114411598c8d68850d73f6ce7877bb6bcc0f
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2021-07-16 00:49:08 -0300

    funcionalidade isActive nos botoes de tipo

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -49,30 +49,35 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color 0.2s;
+`;
 
-    &:hover {
-      border-color: #c1c1c1;
-    }
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    img { 
-      width: 20px;
-      height: 20px;
-    }
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => props.isActive ? '#e7e9ee' : 'transparent'};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-tittle);
-    }
+  &:hover {
+    border-color: #c1c1c1;
+  }
+
+  img { 
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-tittle);
   }
 `;
